f18d2579283d0723a49ebe13c5bbdfd62a8a7bcc
Ciarango10 <carlosarango3101@gmail.com>
2024-09-07 12:03:46 -0500

    Implementacion de las rutas con react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-
+import {Routes, Route} from "react-router-dom"
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import Home from './Routes/Home'
+import Contact from './Routes/Contact'
+import Detail from './Routes/Detail'
+import Favs from './Routes/Favs'
+import { routes } from "./Components/utils/routes";
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
+          <Routes>
+              <Route path={routes.home} element={<Home />} />
+              <Route path={routes.contact} element={<Contact/>} />
+              <Route path="detail/:id" element={<Detail/>}/>
+              <Route path={routes.favs} element={<Favs/>} />
+              <Route path={routes.notFound} element={<h1>Error 404 - Page not Found</h1>}/>
+          </Routes>
           <Footer/>
       </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import { routes } from "./utils/routes";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -6,7 +8,9 @@ const Navbar = () => {
 
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <Link to={routes.home}>Inicio</Link>
+      <Link to={routes.contact}>Contacto</Link>
+      <Link to={routes.favs}>Destacados</Link>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
     </nav>
