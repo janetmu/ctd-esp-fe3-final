df39133befb0e0671bcf8b08f4ba597a4a06f8a4
Janet Marulanda <janet_marulanda@yahoo.com>
2024-09-08 11:18:45 -0500

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
+import styles from "../Styles/Footer.module.css";
 
 const Footer = () => {
   return (
     <footer>
+      <div>
         <p>Powered by</p>
-        <img src="./img/DH.png" alt='DH-logo' />
+        <img src="./images/DH.png" alt="DH-logo" />
+      </div>
+      <div>
+        <a href="#">
+          <img src="/imagenes/ico-facebook.png" alt="Facebook" />
+        </a>
+        <a href="#">
+          <img src="/imagenes/ico-instagram.png" alt="Instagram" />
+        </a>
+        <a href="#">
+          <img src="/imagenes/ico-whatsapp.png" alt="WhatsApp" />
+        </a>
+        <a href="#">
+          <img src="/imagenes/ico-tiktok.png" alt="TikTok" />
+        </a>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+/*import React from "react";
+import styles from "../Styles/Navbar.module.css";
+import { Link } from "react-router-dom";
+import { useContextGlobalStates} from "./utils.global.context";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
+  const { state, dispatch } = useContextGlobalStates();
+
+  const toggleTheme = () => {
+    dispatch({ type: "TOGGLE_THEME" });
+  };
 
   return (
-    <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+    <nav
+      className={`${styles.navbar} ${
+        state.theme === "dark" ? styles.dark : styles.light
+      }`}
+    >
+      <ul className={styles.navLinks}>
+        <li>
+          <Link to="/" className={styles.link}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className={styles.link}>
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link to="/favs" className={styles.link}>
+            Favs
+          </Link>
+        </li>
+      </ul>
+      <button onClick={toggleTheme} className={styles.themeButton}>Change theme</button>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;*/
