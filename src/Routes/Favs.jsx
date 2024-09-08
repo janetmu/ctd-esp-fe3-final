c54e6de08d9e6a0521f3caf98a9411cb752b12d0
Ciarango10 <carlosarango3101@gmail.com>
2024-09-07 20:32:14 -0500

    Pagina favs con localStorage

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Components/Card";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
+  const favs = JSON.parse(localStorage.getItem("favs")) || [];
 
   return (
     <>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
+        {
+          favs.map((dentist) => {
+            return <Card name={dentist.name} username={dentist.username} id={dentist.id} key={dentist.id} />
+          })
+        }
       </div>
     </>
   );
