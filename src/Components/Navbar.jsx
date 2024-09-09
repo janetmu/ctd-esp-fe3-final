1050773074adbaba70f89ecf0891d632d2171cc8
Janet Marulanda <janet_marulanda@yahoo.com>
2024-09-09 15:39:32 -0500

    se crea navbar: compponente y estilos

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import { routes } from "./utils/routes";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Brightness4, NightlightRound } from "@mui/icons-material"; // Importamos los iconos
 
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+import styles from "../Styles/Navbar.module.css";
+import { useContextGlobalStates } from "./utils/global.context";
 
 const Navbar = () => {
+  const { state, dispatch } = useContextGlobalStates();
+
+  const toggleTheme = () => {
+    dispatch({ type: "TOGGLE_THEME" });
+  };
 
   return (
-    <nav>
-      <Link to={routes.home}>Inicio</Link>
-      <Link to={routes.contact}>Contacto</Link>
-      <Link to={routes.favs}>Destacados</Link>
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
-    </nav>
-  )
-}
+    <nav
+      className={`${styles.navbar} ${
+        state.theme === "dark" ? styles.dark : styles.light
+      }`}
+    >
+      <Link to="/" className={styles.logoContainer}>
+        <h1 className={styles.title}>
+          <span className={styles.red}>D</span>H Odonto
+        </h1>
+      </Link>
+      <div className={styles.rightSection}>
+        <div className={styles.navLinks}>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/favs">Favs</Link>
+        </div>
 
-export default Navbar
+        {/* Botón para cambiar el tema */}
+        <button className={styles.themeButton} onClick={toggleTheme}>
+          {state.theme === "dark" ? (
+            <Brightness4 />
+          ) : (
+            <NightlightRound className={styles.flippedIcon} />
+          )}{" "}
+          {/* Icono según el tema */}
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
