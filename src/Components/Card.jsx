4357c0023fa65159b35a012f04e43342cc429118
Janet Marulanda <janet_marulanda@yahoo.com>
2024-09-09 22:54:22 -0500

    se cambia texto boton cards por icono estrella

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CardStyles from "../Styles/Card.module.css";
 import { Link } from "react-router-dom";
+import Star from "@mui/icons-material/StarPurple500Sharp";
 
 localStorage.setItem("favs", JSON.stringify([]));
 
@@ -13,13 +14,17 @@ const Card = ({ name, username, id }) => {
   }
 
   return (
-    <div className={CardStyles.card}>
-      <Link key={id} to={`detail/${id}`}>
-        <img src="/images/doctor.jpg" alt="Dentist profile picture" />
-        <h2>{name}</h2>
-        <p>{username}</p>
-      </Link>
-      <button onClick={addFav} className={CardStyles.favButton}>Add fav</button>
+    <div className={CardStyles.cardsContainer}>
+      <div className={CardStyles.card}>
+        <Link key={id} to={`detail/${id}`}>
+          <img src="/images/doctor.jpg" alt="Dentist profile picture" />
+          <h2>{name}</h2>
+          <p>{username}</p>
+        </Link>
+        <button onClick={addFav} className={CardStyles.favButton}>
+          <Star className={CardStyles.starIcon} />
+        </button>
+      </div>
     </div>
   );
 };
